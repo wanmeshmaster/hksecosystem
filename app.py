e292e5bc8984731166a7efcd82764a03df973ffc
Gary Davis <hugewanmesh@gmail.com>
2026-06-25 11:16:45 +0100

    version 0.2.0
    - added template for "HKS Bank"

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 from flask import Flask, render_template
 
-# Initialize Flask and tell it to serve static files from the 'source' folder
 app = Flask(__name__, static_folder='source', static_url_path='/source')
 
 @app.route('/')
 def home():
-    # The 'id' will be used to look for the .jpg file.
+    # We added the HKS Bank app here with the ID 'hks-bank'
     apps = [
         {
             'id': 'coming-soon', 
             'title': 'Coming soon...', 
             'url': '/coming-soon.html'
+        },
+        {
+            'id': 'hks-bank', 
+            'title': 'HKS Bank', 
+            'url': '/hks-bank.html'
         }
     ]
     
@@ -25,12 +29,16 @@ def home():
         else:
             a['bg_image'] = None
 
-    # Pass the apps list to the HTML template
     return render_template('index.html', apps=apps)
 
 @app.route('/coming-soon.html')
 def coming_soon():
     return render_template('coming-soon.html')
 
+# New route for the HKS Bank template
+@app.route('/hks-bank.html')
+def hks_bank():
+    return render_template('hks-bank.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True) # Assuming you changed it to 8080
